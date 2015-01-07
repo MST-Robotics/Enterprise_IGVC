2a33b7f9b84f5256c362645d759a3680abb56678
Islam Elnabarawy <ie3md@mst.edu>
2015-01-07 15:24:10 -0600

    Update motor controller arduino code to include the light blinking without delay.

--- arduino_files/motor/motor.ino
+++ arduino_files/motor/motor.ino
@@ -1,63 +1,117 @@
+// motor.ino
+// Arduino sketch for controlling the motors and blinking the indicator light
+
 #include <ros.h>
 
 #include <mst_control/Velocity.h>
 #include <std_msgs/UInt8.h>
+
 
 /*******************************************************************************
  * Variables
  *******************************************************************************/
 
 uint8_t lightMode = 0;
+unsigned long lightLastMillis = 0;
+bool lightState = LOW;
 
 ros::NodeHandle nh;
+
 
 /*******************************************************************************
  * Constants
  *******************************************************************************/
 
-//Pin configurations
-const int LEFT_VELOCITY = 10;
-const int RIGHT_VELOCITY = 11;
-const int LEFT_DIRECTION = 12;
-const int RIGHT_DIRECTION = 13;
-const int LIGHT = 7;
+// Pin configurations
+const int PIN_LEFT_VELOCITY = 10;
+const int PIN_RIGHT_VELOCITY = 11;
+const int PIN_LEFT_DIRECTION = 12;
+const int PIN_RIGHT_DIRECTION = 13;
+const int PIN_LIGHT = 7;
+
+// light update interval
+const unsigned long LIGHT_INTERVAL = 1000; // 1000 ms = 1 second
+
+
+/*******************************************************************************
+ * Callbacks
+ *******************************************************************************/
 
 // Callback for Velocity which gets the left and right wheel velocity and direction
 void VelocityCallback(const mst_control::Velocity &msg) {
-    analogWrite(LEFT_VELOCITY, msg.left_vel);
-    analogWrite(RIGHT_VELOCITY, msg.right_vel);
-    digitalWrite(LEFT_DIRECTION, !msg.left_dir);
-    digitalWrite(RIGHT_DIRECTION, msg.right_dir);
+    analogWrite(PIN_LEFT_VELOCITY, msg.left_vel);
+    analogWrite(PIN_RIGHT_VELOCITY, msg.right_vel);
+    digitalWrite(PIN_LEFT_DIRECTION, !msg.left_dir);
+    digitalWrite(PIN_RIGHT_DIRECTION, msg.right_dir);
 }
 
-//Callback from LIGHT msg, changes mode of the LIGHT
+// Callback from light msg, changes mode of the light
 void LightCallback(const std_msgs::UInt8 &msg) {
     lightMode = msg.data;
 }
 
+
+/*******************************************************************************
+ * Update Methods
+ *******************************************************************************/
+
 void updateLight() {
-    
+    if (lightMode == 1) {
+        // blink without delay
+        // based on: http://arduino.cc/en/Tutorial/BlinkWithoutDelay
+        unsigned long currentMillis = millis();
+         
+        if((currentMillis - lightLastMillis) > LIGHT_INTERVAL) {
+            // save the last time we blinked the LED 
+            lightLastMillis  = currentMillis;
+            
+            // if the light is off turn it on and vice-versa:
+            if (lightState == LOW)
+                lightState = HIGH;
+            else
+                lightState = LOW;
+            
+            // set the output value on the light pin
+            digitalWrite(PIN_LIGHT, lightState);
+        }
+    } else {
+        // make sure the light is on solid
+        if (lightState == LOW) {
+            lightState = HIGH;
+            digitalWrite(PIN_LIGHT, lightState);
+        }
+    }
 }
+
+
+/*******************************************************************************
+ * Initial Setup
+ *******************************************************************************/
 
 ros::Subscriber<mst_control::Velocity> velocitySub("cmd_vel", &VelocityCallback);
 ros::Subscriber<std_msgs::UInt8> lightSub("indicator_light", &LightCallback);
 
 void setup() {
-    pinMode(LEFT_VELOCITY, OUTPUT);
-    pinMode(RIGHT_VELOCITY, OUTPUT);
-    pinMode(LEFT_DIRECTION, OUTPUT);
-    pinMode(RIGHT_DIRECTION, OUTPUT);
-    pinMode(LIGHT, OUTPUT);
+    pinMode(PIN_LEFT_VELOCITY, OUTPUT);
+    pinMode(PIN_RIGHT_VELOCITY, OUTPUT);
+    pinMode(PIN_LEFT_DIRECTION, OUTPUT);
+    pinMode(PIN_RIGHT_DIRECTION, OUTPUT);
+    pinMode(PIN_LIGHT, OUTPUT);
     
-    digitalWrite(LIGHT, LOW);
+    digitalWrite(PIN_LIGHT, LOW);
     
     nh.initNode();
     nh.subscribe(velocitySub);
     nh.subscribe(lightSub);
 }
 
+
+/*******************************************************************************
+ * Main Loop
+ *******************************************************************************/
+
 void loop() {
     nh.spinOnce();
-    delay(10);
+    updateLight();
 }
 
