c8e73c95bf9886e71ce7067a2d86e7c0a12b68c9
Matt Anderson <mia2n4@mst.edu>
2015-02-06 20:33:27 -0600

    Add enterprise_description package
    In order to use some of the more advanced ros features it is important
    that enterprise have a transform tree which describes it.  The tf
    package documentation can be found at: http://wiki.ros.org/tf.
    
    This commit includes a node which takes data from the imu and transmits
    changes in orientation to the odom frame.  This should be the first
    step to implementing gmapping/SLAM.  It will be important to combine
    this rotation data with translation data for SLAM to do more than map
    in a circle.

---
+++ enterprise_description/src/tf_transforms.cpp
@@ -0,0 +1,82 @@
+/*******************************************************************************
+* @file tf_transforms.cpp
+* @brief This contains the transforms for enterprise and will take in sensor 
+*        data and publish joint states
+* @author Matt Anderson <mia2n4@mst.edu>
+*******************************************************************************/
+
+#include <string>
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Quaternion.h>
+#include <tf/transform_broadcaster.h>
+
+//Global Constants
+double heading;
+
+//Callback functions
+/***********************************************************
+ * @fn imuCallback(const geometry_msgs::Quaternion::ConstPtr
+ * & imu)
+ * @brief gets the position information from the imu 
+ * (magnatometer), sets the current_head = heading 
+ * observed by the magnatometer.
+ * @pre imu publishing Quaternion
+ * @post current_head = robot's heading
+ * @param takes in a imu publishing Quaternion
+ ***********************************************************/
+void imu_callback(const geometry_msgs::Quaternion::ConstPtr& imu) 
+{
+    heading = atan2(imu->y, imu->x);
+}
+
+int main(int argc, char** argv)
+{
+    //Ros initialization
+    ros::init(argc, argv, "state_publisher");
+    ros::NodeHandle n;
+    
+    //Create subscriber to IMU data
+    ros::Subscriber imu_sub;
+    imu_sub = n.subscribe<geometry_msgs::Quaternion>("/imu", 20, &imu_callback);
+    
+    ros::Publisher joint_pub;
+    joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+    tf::TransformBroadcaster broadcaster;
+    ros::Rate loop_rate(30);
+    
+    // message declarations
+    geometry_msgs::TransformStamped odom_trans;
+    sensor_msgs::JointState joint_state;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_link";
+    
+    while(ros::ok())
+    {
+        //update joint_state
+        joint_state.header.stamp = ros::Time::now();
+        joint_state.name.resize(3);
+        joint_state.position.resize(3);
+        joint_state.name[0] ="Camera";
+        joint_state.position[0] = 0;
+        joint_state.name[1] ="lidar_link";
+        joint_state.position[1] = 0;
+
+        // update transform
+        // (moving in a circle with radius=2)
+        odom_trans.header.stamp = ros::Time::now();
+        odom_trans.transform.translation.x = 0;
+        odom_trans.transform.translation.y = 0;
+        odom_trans.transform.translation.z = 0;
+        odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(heading);
+
+        //send the joint state and transform
+        joint_pub.publish(joint_state);
+        broadcaster.sendTransform(odom_trans);
+
+        // This will adjust as needed per iteration
+        ros::spinOnce();
+    }
+    
+    return 0;
+}
